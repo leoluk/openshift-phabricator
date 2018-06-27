967aa61c99d046291d2c43d0489bdc6fdea41003
Leopold Schabel <mail@leoschabel.de>
2018-06-27 13:36:38 +0200

    Set local paths

--- php-pre-start/pre-start.sh
+++ php-pre-start/pre-start.sh
@@ -5,5 +5,8 @@ phabricator/bin/config set mysql.host ${DATABASE_SERVICE_HOST}
 phabricator/bin/config set mysql.user ${DATABASE_SERVICE_USER}
 phabricator/bin/config set mysql.pass ${DATABASE_SERVICE_PASSWORD}
 
+phabricator/bin/config set repository.default-local-path ${APP_ROOT}/repositories
+phabricator/bin/config set storage.local-disk.path ${APP_ROOT}/files
+
 echo "Running database migrations..."
 phabricator/bin/storage upgrade --force
