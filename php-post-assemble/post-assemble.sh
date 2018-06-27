d30ddccdbc9016e4841bcbdb8272d26e85086cf5
Leopold Schabel <mail@leoschabel.de>
2018-06-27 21:58:52 +0200

    Found a better way to disable opcache

--- php-post-assemble/post-assemble.sh
+++ php-post-assemble/post-assemble.sh
@@ -6,3 +6,5 @@ sed -i "s%^\$root =.*$%\$root = '${APP_ROOT}/src/phabricator';%" phabricator/bin
 # Custom preamble which forces HTTPS
 cp preamble.php phabricator/support/preamble.php
 
+# Disable opcache revalidation
+sed -i 's/opcache.validate_timestamps=.*/opcache.validate_timestamps=0/' ${APP_ROOT}/etc/php.d/10-opcache.ini.template
