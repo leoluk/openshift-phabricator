02af5c75e91c32b0df331baf85fbe7202b198f34
Leopold Schabel <mail@leoschabel.de>
2018-06-27 15:28:07 +0200

    Be more sophisticated about replacing $root

--- php-post-assemble/post-assemble.sh
+++ php-post-assemble/post-assemble.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Confusion due to symlinks in the path
-sed -i 's/dirname(dirname(dirname/dirname(dirname(/' phabricator/bin/*
+sed -i "s%^\$root =.*$%\$root = '${APP_ROOT}/src/phabricator';%" phabricator/bin/*
 
 # Custom preamble which forces HTTPS
 cp preamble.php phabricator/support/preamble.php
