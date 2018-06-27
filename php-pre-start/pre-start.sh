588fee5ffd22a49e5dbfe6d3f161ea2a787c3f36
Leopold Schabel <mail@leoschabel.de>
2018-06-27 15:49:38 +0200

    Aphlict config in Phabricator

--- php-pre-start/pre-start.sh
+++ php-pre-start/pre-start.sh
@@ -8,5 +8,25 @@ phabricator/bin/config set mysql.pass ${DATABASE_SERVICE_PASSWORD}
 phabricator/bin/config set repository.default-local-path ${APP_ROOT}/repositories
 phabricator/bin/config set storage.local-disk.path ${APP_ROOT}/files
 
+if [[ -z $APHLICT_HOST ]]; then
+  echo "APHLICT_HOST not set, skipping"
+else
+  phabricator/bin/config set notification.servers '[
+  {
+    "type": "client",
+    "host": "'${APHLICT_HOST}'",
+    "port": 443,
+    "protocol": "https"
+  },
+  {
+    "type": "admin",
+    "host": "'${APHLICT_SERVICE}'",
+    "port": 22281,
+    "protocol": "http"
+  }
+]'
+fi
+
 echo "Running database migrations..."
 phabricator/bin/storage upgrade --force
+
