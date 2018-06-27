aebb7fbb156ea00fefef6d80686d563dcfc794ee
Leopold Schabel <mail@leoschabel.de>
2018-06-27 13:40:37 +0200

    Add comments

--- php-post-assemble/post-assemble.sh
+++ php-post-assemble/post-assemble.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Confusion due to symlinks in the path
 sed -i 's/dirname(dirname(dirname/dirname(dirname(/' phabricator/bin/*
 
+# Custom preamble which forces HTTPS
 cp preamble.php phabricator/support/preamble.php
+
